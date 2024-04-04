95cab8feb5c0f15eb03e0323a413814237b05260
kionpark <kionpark@gmail.com>
2024-04-04 11:26:29 +0900

    nxt

--- app/page.jsx
+++ app/page.jsx
@@ -1,13 +1,34 @@
 import { connectDB } from "@/util/database.js"
+import 이미지 from '/public/dogi.png';
+import Image from "next/image";
 
 export default async function Home() {
-  let client = await connectDB();
-  const db = client.db('forum');
-  let result = await db.collection('post').find().toArray();
+  // let client = await connectDB();
+  // const db = client.db('forum');
+  // let result = await db.collection('post').find().toArray();
 
+  let 상품 = ['Tomatoes', 'Pasta', 'Coconut']
   return (
-    <main>
-      메인 페이지임
-    </main>
+    <div>
+      <h4 className="title">상품목록</h4>
+      {/* <div className="food"> 
+        <h4>{ 상품[0] } $40</h4>
+      </div>
+      <div className="food"> 
+        <h4>{ 상품[1] } $40</h4>
+      </div>
+      <div className="food"> 
+        <h4>{ 상품[2] } $40</h4>
+      </div> */}
+      {상품.map((product, idx) => {
+        console.log(product);
+        return (
+          <h4 key={idx}>{product} $40</h4>
+        )
+      })}
+
+      <Image src={이미지} alt="설명" />
+
+    </div>
   )
 }
